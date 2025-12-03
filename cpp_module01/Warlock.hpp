da43a51d79a1538b47a92189699eae9eeb3f3920
Mohammed Ehthisham Ullah Khan <mohkhan@lab1r5s11.42abudhabi.ae>
2025-12-03 18:57:05 +0400

    05

---
+++ cpp_module01/Warlock.hpp
@@ -0,0 +1,11 @@
+#ifndef WARLOCK_HPP
+#define WARLOCCK_HPP
+
+#include <iostream>
+
+class Warlock{
+    private:
+        Warlock();
+        Warlock(std::string name, std::string title);
+        Warlock()
+}
